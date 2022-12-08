ce9dc59be7236a6ce9dbc307b5432e12b32fa63f
Jiwoo Jeong <111746601+jiwoojeong17@users.noreply.github.com>
2022-12-08 07:34:50 +0000

    add popup for join/leave and contact info

--- src/pages/DetailPost.js
+++ src/pages/DetailPost.js
@@ -13,22 +13,75 @@ import {
   IonTextarea,
   IonInput,
   IonIcon,
-  IonFab,
-  IonFabButton,
-  IonAlert,
   IonHeader,
   IonButtons,
   IonBackButton,
   IonTitle,
   IonToolbar,
+  IonPopover,
+  useIonToast
 } from "@ionic/react";
-import { fastFood, paperPlane, trash } from "ionicons/icons";
+import { fastFood, paperPlane } from "ionicons/icons";
 import "./DetailPost.css";
 
 import { useState } from "react";
 import Tab from "../components/Tab";
+import serverRequest from "../common";
+import { useParams } from "react-router";
+
+const placeHolder = {
+  title: "Let's eat!",
+  writer: "Mr.Kim",
+  content: "I want to delivery chicken...\nSo so...\nFinally...",
+  product: "ABC Chicken",
+  recruitsNo: 4,
+  datetime: "2022-11-27T17:00",
+  place: "XYZ dormitory",
+  price: 18000,
+  contactInfo: "kim@kaist.ac.kr",
+};
 
 const DetailPost = () => {
+  const { id } = useParams();
+
+  serverRequest("/login/", "POST", {
+    id: "test@kaist.ac.kr",
+    password: "test",
+  }).then();
+
+  const [post, setPost] = useState(placeHolder);
+  const [comments, setComments] = useState([]);
+
+  serverRequest(`/post/food/${id}`, "GET")
+    .then((r) => r.json())
+    .then((r) => {
+      if (post != placeHolder) return;
+      console.log(r);
+
+      const newPost = {
+        title: r.title,
+        writer: r.nickname,
+        content: r.content,
+        product: r.product,
+        recruitsNo: r.capacity,
+        datetime: r.time,
+        place: r.place,
+        price: r.price,
+        contactInfo: "fixme",
+      };
+      setPost(newPost);
+
+      const newComments = r.comments.map((comment) => {
+        return {
+          writer: comment.nickname,
+          content: comment.content,
+        };
+      });
+
+      setComments(newComments);
+    });
+
+  /*
   // dummy post data
   const ContentPost = {
     title: "Let's eat!",
@@ -64,16 +117,19 @@ const DetailPost = () => {
     commentId: "2",
   };
   const commentList = [Comment1, Comment2];
+  */
 
   // for create new comment
   const [newCommentWriter, setNewCommentWriter] = useState([]);
   const [newCommentContent, setNewCommentContent] = useState([]);
   const submitNewComment = () => {
-    const _newComment = {
-      writer: newCommentWriter,
-      comment: newCommentContent,
-    };
-    console.log(_newComment);
+    serverRequest(`/post/food/${id}/comment`, "POST", {
+      nickname: "fixMeComment",
+      content: newCommentContent,
+    }).then(() => {
+      console.log("comment sent");
+      window.location.reload();
+    });
   };
 
   // for delete comment
@@ -86,16 +142,40 @@ const DetailPost = () => {
     console.log(_deleteComment);
   };
 
+  const [presentToast] = useIonToast();
+  const [handlerMessage, setHandlerMessage] = useState('');
+  const [roleMessage, setRoleMessage] = useState('');
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
-            <IonBackButton defaultHref="./Food"></IonBackButton>
+            <IonBackButton defaultHref="/Food"></IonBackButton>
           </IonButtons>
           <IonTitle id="board_title">
             <IonIcon class="icon" icon={fastFood}></IonIcon>Food Delivery
           </IonTitle>
+          <IonButtons slot="end">
+            <IonButton
+            onClick={() => {
+              presentToast({
+                message: 'Participate:',
+                duration: 5000,
+                buttons: [
+                  {
+                    text: 'Join',
+                    // handler: () => { setHandlerMessage('More Info clicked'); }
+                  },
+                  {
+                    text: 'Leave',
+                    // handler: () => { setHandlerMessage('Dismiss clicked'); }
+                  }
+                ]
+              })
+            }}
+          >Join</IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
 
@@ -104,15 +184,15 @@ const DetailPost = () => {
           <IonCardHeader>
             <IonItem lines="none">
               <IonCardTitle>
-                <IonInput value={title} readonly={true}></IonInput>
+                <IonInput value={post.title} readonly={true}></IonInput>
               </IonCardTitle>
-              <IonButton slot="end" href="./EditPost">
+              <IonButton slot="end" href="/EditPost">
                 Edit
               </IonButton>
             </IonItem>
             <IonItem lines="none">
               <IonCardSubtitle>
-                <IonInput value={writer} readonly={true}></IonInput>
+                <IonInput value={post.writer} readonly={true}></IonInput>
               </IonCardSubtitle>
             </IonItem>
           </IonCardHeader>
@@ -122,7 +202,7 @@ const DetailPost = () => {
               <IonItem lines="none">
                 <IonTextarea
                   id="PostContent"
-                  value={content}
+                  value={post.content}
                   readonly={true}
                   autoGrow={true}
                   rows="1"
@@ -133,43 +213,46 @@ const DetailPost = () => {
                 <IonLabel>
                   <b>Product</b>
                 </IonLabel>
-                <IonInput value={product} readonly={true}></IonInput>
+                <IonInput value={post.product} readonly={true}></IonInput>
               </IonItem>
 
               <IonItem lines="none">
                 <IonLabel>
                   <b>Time</b>
                 </IonLabel>
-                <IonInput value={datetime} readonly={true}></IonInput>
+                <IonInput value={post.datetime} readonly={true}></IonInput>
               </IonItem>
 
               <IonItem lines="none">
                 <IonLabel>
                   <b>Place</b>
                 </IonLabel>
-                <IonInput value={place} readonly={true}></IonInput>
+                <IonInput value={post.place} readonly={true}></IonInput>
               </IonItem>
 
               <IonItem lines="none">
                 <IonLabel>
                   <b>Price</b>
                 </IonLabel>
-                <IonInput value={price} readonly={true}></IonInput>
+                <IonInput value={post.price} readonly={true}></IonInput>
               </IonItem>
 
               <IonItem lines="none">
                 <IonLabel>
                   <b>Participants</b>
                 </IonLabel>
-                <IonInput value={recruitsNo} readonly={true}></IonInput>
+                <IonInput value={post.recruitsNo} readonly={true}></IonInput>
               </IonItem>
 
               <IonItem lines="none">
                 <IonLabel>
                   <b>Contact Information</b>
                 </IonLabel>
-                <IonInput value={contactInfo} readonly={true}></IonInput>
-              </IonItem>
+                <IonButton id="click-trigger" slot="end">Check info</IonButton>
+                <IonPopover trigger="click-trigger" triggerAction="click">
+                  <IonInput value={post.contactInfo} readonly={true}></IonInput>
+                </IonPopover>
+              </IonItem>       
             </IonList>
           </IonCardContent>
         </IonCard>
@@ -177,37 +260,11 @@ const DetailPost = () => {
         <IonCard>
           <IonCardContent>
             <IonList>
-              {commentList.map((comment) => (
-                <IonList key={comment.commentId}>
-                  <IonFab vertical="top" horizontal="end">
-                    <IonFabButton 
-                      size="small" 
-                      color="medium" 
-                      id="commentMenu"
-                      onClick={() => {
-                        setDeleteCommentAlert(true);
-                        setDeleteComment();
-                        }}>
-                      <IonAlert
-                        isOpen={deleteCommentAlert}
-                        onDidDismiss={() => setDeleteCommentAlert(false)}
-                        header="Delete this comment"
-                        message="Continue?"
-                        buttons={[{
-                          text: 'Cancel'
-                          },
-                          {text: 'Delete',
-                          handler: () => {submitDeleteComment()},
-                        }]}/>
-                      <IonIcon
-                        icon={trash}
-                        id="commentIcon"
-                      ></IonIcon>
-                    </IonFabButton>
-                  </IonFab>
+              {comments.map((comment, index) => (
+                <IonList key={index}>
                   <h2 id="CommentWriter">{comment.writer}</h2>
                   <IonTextarea
-                    value={comment.comment}
+                    value={comment.content}
                     readonly={true}
                     autoGrow={true}
                     rows="1"
@@ -228,8 +285,8 @@ const DetailPost = () => {
               <IonInput
                 onIonChange={(e) => {
                   setNewCommentWriter(e.detail.value);
-                  }}>
-              </IonInput>
+                }}
+              ></IonInput>
             </IonItem>
             <IonItem>
               <IonTextarea
@@ -237,7 +294,8 @@ const DetailPost = () => {
                 rows="1"
                 onIonChange={(e) => {
                   setNewCommentContent(e.detail.value);
-                }}>
+                }}
+              >
                 <IonLabel position="floating">Comment</IonLabel>
               </IonTextarea>
             </IonItem>
